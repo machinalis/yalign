a4a60fa1badd2fa262d0d8d067fd4641c3781fd1
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2013-07-15 17:51:59 -0300

    Improvement on words score to use the direct translations

--- yalign/wordpairscore.py
+++ yalign/wordpairscore.py
@@ -13,8 +13,6 @@ class WordPairScore(ScoreFunction):
 
     def _parse_words_file(self):
         # FIXME: Add support for .csv.gz
-        # FIXME: Question: Why not (word_a, word_b) as keys?
-        # FIXME: Implement the reverse on the outside
         data = csv.reader(open(self.filepath))
         for elem in data:
             word_a, word_b, prob = elem
@@ -27,12 +25,16 @@ class WordPairScore(ScoreFunction):
     def __call__(self, sentence_a, sentence_b):
         result = []
         values = {}
+        set_a = set()
         for word_a in sentence_a:
             word_a = word_a.lower()
+            set_a.add(word_a)
             if word_a in self.translations:
                 values.update(self.translations[word_a])
         for word_b in sentence_b:
             word_b = word_b.lower()
             if word_b in values:
                 result.append(values[word_b])
+            elif len(word_b) > 2 and word_b in set_a:
+                result.append(1.0)
         return result
