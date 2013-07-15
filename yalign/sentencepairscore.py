728b01372d3fa268eedf5fd19109cb1f8a79f613
Gonzalo Garcia Berrotaran <ggarcia@machinalis.com>
2013-07-15 11:27:19 -0300

    Using only a phew features and with cache

--- yalign/sentencepairscore.py
+++ yalign/sentencepairscore.py
@@ -3,8 +3,8 @@
 import math
 from simpleai.machine_learning import ClassificationProblem, is_attribute
 
-from yalign.datatypes import ScoreFunction, SentencePair
 from yalign.svm import SVMClassifier
+from yalign.datatypes import ScoreFunction, SentencePair
 
 
 class SentencePairScore(ScoreFunction):
@@ -13,7 +13,6 @@ class SentencePairScore(ScoreFunction):
         self.classifier = None
         self.sign = 1
 
-    # FIXME: Consider giving the word_aligner instead of word_score_function
     def train(self, pairs, word_score_function):
         """
         Trains the sentence pair likelihood score using examples.
@@ -52,7 +51,6 @@ class SentencePairScore(ScoreFunction):
         a = SentencePair(a, b)
         score = self.classifier.score(a) * self.sign
         result = logistic_function(score * 3)
-        # FIXME: Consider moving this to a test
         assert self.min_bound <= result <= self.max_bound
         return result
 
@@ -64,13 +62,10 @@ class SentencePairScore(ScoreFunction):
 class SentencePairScoreProblem(ClassificationProblem):
     def __init__(self, word_pair_score):
         super(SentencePairScoreProblem, self).__init__()
-        # If gap > 0.5 then the returned value could be > 1.
         self.word_pair_score = word_pair_score
-
-    @is_attribute
-    def position_difference(self, alignment):
-        d = alignment.a.position - alignment.b.position
-        return abs(d)
+        translations = self.word_pair_score.translations
+        word_translations = WordsTranslations(translations)
+        self.word_translations = CacheOfSizeOne(word_translations)
 
     @is_attribute
     def word_length_difference(self, alignment):
@@ -79,101 +74,13 @@ class SentencePairScoreProblem(ClassificationProblem):
         return ratio(a, b)
 
     @is_attribute
-    def uppercase_words_difference(self, alignment):
-        a = len([x for x in alignment.a if x.isupper()])
-        b = len([x for x in alignment.b if x.isupper()])
-        return ratio(a, b)
-
-    @is_attribute
-    def capitalized_words_difference(self, alignment):
-        a = len([x for x in alignment.a if x.istitle()])
-        b = len([x for x in alignment.b if x.istitle()])
-        return ratio(a, b)
-
-    @is_attribute
-    def commas(self, alignment):
-        a = len([x for x in alignment.a if x == ","])
-        b = len([x for x in alignment.b if x == ","])
-        return ratio(a, b)
-
-    @is_attribute
-    def question_marks(self, alignment):
-        a = len([x for x in alignment.a if x == "?"])
-        b = len([x for x in alignment.b if x == "?"])
-        return ratio(a, b)
-
-    @is_attribute
-    def punctuation(self, alignment):
-        punc = ",.-?+<>_\\/"
-        a = len([x for x in alignment.a if x in punc])
-        b = len([x for x in alignment.b if x in punc])
-        return ratio(a, b)
-
-    @is_attribute
-    def digits(self, alignment):
-        a = len([x for x in alignment.a if x.isdigit()])
-        b = len([x for x in alignment.b if x.isdigit()])
-        return ratio(a, b)
-
-    @is_attribute
-    def word_match(self, alignment):
-        a, b = list(alignment.a), list(alignment.b)
-        diff = abs(len(a) - len(b))
-        if diff > 2 * len(a) or diff > 2 * len(b):
-            return 1
-        score = 0
-        for cost, word_a, word_b in self._word_pairs(alignment):
-            if word_a in a and word_b in b:
-                score += cost
-                a.remove(word_a)
-                b.remove(word_b)
-        score += len(a) + len(b)
-        return float(score) / max(len(alignment.a), len(alignment.b))
-
-    def _word_pairs(self, sentencepair):
-        a, b = sentencepair
-        for word_a in a:
-            item = []
-            max_score = 1
-            for word_b in b:
-                cost = self.word_pair_score(word_a, word_b)
-                if cost < max_score:
-                    max_score = cost
-                    item = (max_score, word_a, word_b)
-            if item:
-                yield item
-
-    #@is_attribute
     def linear_word_match(self, alignment):
-        values = {}
-        translations = self.word_pair_score.translations
-        for word_a in alignment.a:
-            word_a = word_a.lower()
-            if word_a in translations:
-                values.update(translations[word_a])
-
-        total = 0.0
-        for word_b in alignment.b:
-            word_b = word_b.lower()
-            total += values.get(word_b, 0)
-
+        total = sum(self.word_translations(alignment))
         return total / float(max(len(alignment.a), len(alignment.b)))
 
     @is_attribute
     def linear_word_count(self, alignment):
-        values = []
-        translations = self.word_pair_score.translations
-        for word_a in alignment.a:
-            word_a = word_a.lower()
-            if word_a in translations:
-                values.extend(translations[word_a].keys())
-
-        total = 0.0
-        for word_b in alignment.b:
-            word_b = word_b.lower()
-            if word_b in values:
-                total += 1.0
-
+        total = len(self.word_translations(alignment))
         return total / float(max(len(alignment.a), len(alignment.b)))
 
     def target(self, alignment):
@@ -191,6 +98,30 @@ def logistic_function(x):
     See: http://en.wikipedia.org/wiki/Logistic_function
     """
     return 1 / (1 + math.e ** (-x))
+
+
+class WordsTranslations(object):
+    """
+    Using a dictionary of translations in
+    linear time returns a list of probabilities
+    for the words translations.
+    """
+
+    def __init__(self, translations):
+        self.translations = translations
+
+    def __call__(self, alignment):
+        result = []
+        values = {}
+        for word_a in alignment.a:
+            word_a = word_a.lower()
+            if word_a in self.translations:
+                values.update(self.translations[word_a])
+        for word_b in alignment.b:
+            word_b = word_b.lower()
+            if word_b in values:
+                result.append(values[word_b])
+        return result
 
 
 class CacheOfSizeOne(object):
