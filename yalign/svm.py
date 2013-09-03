8e024155fd14e7525be701a95f22b9ecaf38e645
avine <avine@machinalis.com>
2013-09-03 17:32:56 -0300

    patch SVC for different versions of sci-kit learn

--- yalign/svm.py
+++ yalign/svm.py
@@ -5,11 +5,13 @@ Support Vector Machine Classifier
 """
 
 import numpy
+
 from sklearn import svm
 from simpleai.machine_learning import Classifier
 
 
 class SVMClassifier(Classifier):
+
     def learn(self):
         vectors = []
         answers = []
@@ -21,9 +23,11 @@ class SVMClassifier(Classifier):
         if not vectors:
             raise ValueError("Cannot train on empty set")
         self.svm = svm.SVC()
+        self._SVC_hack()
         self.svm.fit(vectors, answers)
 
     def classify(self, data):
+        self._SVC_hack()
         vector = self.vectorize(data)
         return self.svm.predict(vector)[0], 1
 
@@ -31,6 +35,7 @@ class SVMClassifier(Classifier):
         """
         True class is positive, False class is negative.
         """
+        self._SVC_hack()
         vector = self.vectorize(data)
         return float(self.svm.decision_function(vector))
 
@@ -44,4 +49,12 @@ class SVMClassifier(Classifier):
         if "dataset" in result:
             del result["dataset"]
         return result
+
+    def _SVC_hack(self):
+        """
+        This is a dirty hack to deal with SVC's that were pickled using version 0.13.1
+        But the user is now on a later version os scikit-learn where _impl is expected.
+        """
+        if not hasattr(self.svm, '_impl'):
+            self.svm._impl = 'c_svc'
 
